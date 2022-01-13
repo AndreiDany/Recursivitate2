ed34b93397ce31100c72f3e289254c47834f18e4
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2022-01-13 16:03:07 +0200

    Gata

---
+++ Recursivitate2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recursivitate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] { 5, 6, 7, 8, 9};
+            Console.WriteLine(Recursiv.Maxim(vector, vector.Length -1 , int.MinValue));
+        }
+    }
+}
